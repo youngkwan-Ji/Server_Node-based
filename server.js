3f6a9b8772834475427f09e9e9ca8d7554bf3b50
youngkwan-Ji <youngkwanji@gmail.com>
2022-04-18 19:01:10 +0900

    add moment

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const socketLoader = require('./loaders/websocket/socket')
 const define = require('config/define')
 const env = require('config/env');
 // const aggT = require('services/binance/websocket/spot/aggTradeWS');
+// const wsKline = require('services/binance/websocket/spot/klineWS');
 const klines = require('services/binance/spot/market/klines');
 
 
